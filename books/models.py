36129dd9abb925118d0d152b48b12592933b4dd4
fatemearyaie <fatemearyaie9@gmail.com>
2025-09-16 19:06:23 +0330

    edit max_length for isbn and author

--- books/models.py
+++ books/models.py
@@ -4,8 +4,8 @@ from django.db import models
 class Book(models.model):
     title = models.CharField(max_length=250)
     subtitle = models.CharField(max_length=250)
-    author = models.CharField(max_length=250)
-    isbn = models.CharField(max_length=250)
+    author = models.CharField(max_length=100)
+    isbn = models.CharField(max_length=13)
 
     def __str__(self):
         return self.title
