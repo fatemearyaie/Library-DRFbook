1d74c9b705619973481ca0132241b34f0c130ff3
fatemearyaie <fatemearyaie9@gmail.com>
2025-09-19 09:07:12 +0330

    create a view to have books model

--- books/views.py
+++ books/views.py
@@ -1,3 +1,7 @@
 from django.shortcuts import render
-
+from .models import Book
+from django.views.generic import ListView
 # Create your views here.
+class BookList(ListView):
+    model = Book
+    template_name = 'book_list.html'
